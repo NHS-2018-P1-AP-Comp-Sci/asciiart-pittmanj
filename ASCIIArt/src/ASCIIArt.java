6509140d36c89e2775d204861af108bb2b78d26c
s-pittmanj <s-pittmanj@53S50030345.it.bsd405.org>
2018-09-20 08:30:53 -0700

    turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -15,3 +15,78 @@
  */
 
 // YOUR CODE GOES HERE :)
+public class ASCIIArt {
+	public static void main(String[] args) {
+		
+		lCharacter ();
+		spaceCharacter ();
+		oCharacter ();
+		spaceCharacter ();
+		gCharacter ();
+		spaceCharacter ();
+		iCharacter() ;
+		spaceCharacter ();
+		cCharacter();
+		spaceCharacter ();
+		myStatement();
+	}
+	public static void lCharacter () {
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|________");	
+		// this will print a L
+	}
+	
+	public static void oCharacter () {
+		System.out.println("__________");
+		System.out.println("|        |");
+		System.out.println("|        |");
+		System.out.println("|        |");
+		System.out.println("|        |");
+		System.out.println("|        |");
+		System.out.println("|________|");
+		// this will print a O
+	}
+	public static void spaceCharacter () {
+		System.out.println("");
+	}
+	public static void gCharacter () {
+		System.out.println("+________+");
+		System.out.println("|        ");
+		System.out.println("|        ");
+		System.out.println("|     ___");
+		System.out.println("|    /   \\");
+		System.out.println("|        |");
+		System.out.println("+________|");
+		// this will print a G and has one of the 2 special string characters
+	}
+	public static void iCharacter () {
+		System.out.println("      **    ");
+		System.out.println("            ");
+		System.out.println("      ||    ");
+		System.out.println("      ||     ");
+		System.out.println("      ||     ");
+		System.out.println("      ||     ");
+		System.out.println("      ||    ");
+		System.out.println("      ||    ");
+		//this will print a I
+	}
+	public static void cCharacter () {
+		System.out.println("+__________  ");
+		System.out.println("|             ");
+		System.out.println("|            ");
+		System.out.println("|            ");
+		System.out.println("|            ");
+		System.out.println("|          ");
+		System.out.println("+__________  ");
+		// this will print a C
+	}
+	public static void myStatement () {
+		System.out.print(" \" this is the name of my favorite rapper\" ");
+	// this is the other special string character
+	}
+}
